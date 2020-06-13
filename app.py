77283afc6668aafa047db87c2b74f05007005808
g-for7e <56260373+g-for7e@users.noreply.github.com>
2020-06-13 21:41:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,30 @@
 from flask import Flask, request
 import telegram
-from telegram import bot
+#from telegram import bot
 from telebot.credentials import bot_token, bot_user_name,URL
 from telebot.mastermind import get_response
 
 
-global botA
+#global botA
 global TOKEN
 TOKEN = bot_token
-botA = bot(token = TOKEN)
+#botA = telegram.bot(token = TOKEN)
 
 app = Flask(__name__)
 
 @app.route('/{}'.format(TOKEN), methods=['POST'])
 def respond():
     # retrieve the message in JSON and then transform it to Telegram object
-    update = telegram.Update.de_json(request.get_json(force=True), botA)
+    update = telegram.Update.de_json(request.get_json(force=True), telegram.bot(token = TOKEN))
 
     chat_id = update.message.chat.id
     msg_id = update.message.message_id
 
     # Telegram understands UTF-8, so encode text for unicode compatibility
     text_to_be_analyzed = update.message.text.encode('utf-8').decode()
-    print("got text message :", text)
 
     response = get_response(text)
-    botA.sendMessage(chat_id=chat_id, text=response, reply_to_message_id=msg_id)
+    telegram.bot(token = TOKEN).sendMessage(chat_id=chat_id, text=response, reply_to_message_id=msg_id)
 
     return 'ok'
 
