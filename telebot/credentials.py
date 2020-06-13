3ba20c7308e1ea737e27cbff0c6d2b75816d632b
g-for7e <56260373+g-for7e@users.noreply.github.com>
2020-06-13 16:44:57 +0300

    Add files via upload

---
+++ telebot/credentials.py
@@ -0,0 +1,3 @@
+bot_token = "1143220225:AAFTmo2IBybhLYxKNdHlAL-TYdTJHCe4axw"
+bot_user_name = "Dipsupport_bot"
+URL = "https://dipsupport.herokuapp.com/"
---
+++ telebot/mastermind.py
@@ -0,0 +1,29 @@
+import os
+import dialogflow
+from google.api_core.exceptions import InvalidArgument
+
+def get_response(msg):
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = '\\Users\G-FOR7E\Documents\Support-626cf302babc.json'
+
+DIALOGFLOW_PROJECT_ID = 'support-tykfrc'
+DIALOGFLOW_LANGUAGE_CODE = 'ru'
+SESSION_ID = 'gforse'
+
+
+session_client = dialogflow.SessionsClient()
+session = session_client.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
+text_input = dialogflow.types.TextInput(text=text_to_be_analyzed, language_code=DIALOGFLOW_LANGUAGE_CODE)
+query_input = dialogflow.types.QueryInput(text=text_input)
+try:
+    response = session_client.detect_intent(session=session, query_input=query_input)
+except InvalidArgument:
+    raise
+
+detect = response.query_result.intent_detection_confidence
+Final_text = response.query_result.fulfillment_text
+Invalid_out = "Дождитесь ответа специалиста."
+
+if detect = 1.0:
+    return final_text
+else:
+    return Invalid_out
