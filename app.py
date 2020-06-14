34da6e4ff35dbf530ee149bec347036bd6462eed
g-for7e <56260373+g-for7e@users.noreply.github.com>
2020-06-14 12:03:53 +0300

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app = Flask(__name__)
 @app.route('/{}'.format(TOKEN), methods=['POST'])
 def respond():
     # retrieve the message in JSON and then transform it to Telegram object
-    update = telegram.Update.de_json(request.get_json(force=True), telegram.bot(token = TOKEN))
+    update = telegram.Update.de_json(request.get_json(force=True), bot(token = TOKEN))
 
     chat_id = update.message.chat.id
     msg_id = update.message.message_id
@@ -28,14 +28,14 @@ def respond():
     text_to_be_analyzed = update.message.text.encode('utf-8').decode()
 
     response = get_response(text)
-    telegram.bot(token = TOKEN).sendMessage(chat_id=chat_id, text=response, reply_to_message_id=msg_id)
+    bot(token = TOKEN).sendMessage(chat_id=chat_id, text=response, reply_to_message_id=msg_id)
 
     return 'ok'
 
 @app.route('/set_webhook', methods=['GET', 'POST'])
 def set_webhook():
-    s = telegram.bot(token = TOKEN)      #.setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=TOKEN))
-    if s.setWebhook(url='https://dipsupport.herokuapp.com/' + TOKEN):
+    #s = bot(token = TOKEN).setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=TOKEN))
+    if bot(token = TOKEN).setWebhook(url='https://dipsupport.herokuapp.com/' + TOKEN):
         return "webhook setup ok"
     else:
         return "webhook setup failed"
