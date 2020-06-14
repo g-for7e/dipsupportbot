c7b095064a957c4e68acc21e83badb97a64aff6f
g-for7e <56260373+g-for7e@users.noreply.github.com>
2020-06-14 11:51:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -34,8 +34,8 @@ def respond():
 
 @app.route('/set_webhook', methods=['GET', 'POST'])
 def set_webhook():
-#    s = telegram.bot(token = TOKEN).setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=TOKEN))
-    if telegram.bot(token = TOKEN).setWebhook(url='https://dipsupport.herokuapp.com/' + TOKEN):    #.format(URL=URL, HOOK=TOKEN)):
+    s = telegram.bot(token = TOKEN).setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=TOKEN))
+    if s.setWebhook(url='https://dipsupport.herokuapp.com/' + TOKEN):
         return "webhook setup ok"
     else:
         return "webhook setup failed"
